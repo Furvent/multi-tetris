27a485145fe7ebd0aa29a53327e315b60a318a23
Furvent <furventt@gmail.com>
2020-02-29 14:14:36 +0100

    Client: player pseudo - Now player must choose a pseudo before enter in lobby or debug party - Pseudo is stocked in store

--- client/src/views/Tetris.vue
+++ client/src/views/Tetris.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="fur-tetris-home">
-    <div class="fur-options-home" v-if="!userWantToPlayTetris">
+    <div class="fur-options-home" v-if="showOptionsToPlayer">
       <v-btn @click="joinLobbyArea()">Lobby Area</v-btn>
       <v-btn @click="debugLaunchParty()">Debug Launch Party</v-btn>
     </div>
-    <fur-tetris-game-layout v-if="userWantToPlayTetris"></fur-tetris-game-layout>
+    <fur-tetris-game-layout v-if="showLobbyToPlayer"></fur-tetris-game-layout>
     <!-- DIALOG -->
     <v-dialog v-model="showDialogAskPseudo" persistent max-width="500px">
       <v-card>
@@ -12,17 +12,11 @@
           <span>Choose a Pseudo</span>
         </v-card-title>
         <v-card-text>
-          <v-text-field
-            v-model="pseudo"
-            label="Your pseudo"
-            required
-          >
-          </v-text-field>
+          <v-text-field v-model="pseudo" label="Your pseudo" required></v-text-field>
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn>Close</v-btn>
-          <v-btn>Enter</v-btn>
+          <v-btn @click="enterDialog()" :disabled="pseudo === ''">PLAY</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -42,35 +36,48 @@ import FurTetrisGameLayout from "@/components/game-layout/tetris/components/fur-
   }
 })
 export default class extends Vue {
-  pseudo = ""
-  userWantJoinLobby = true;
-  userWantDebug = false;
-  showDialogAskPseudo = false;
-  playerChosePSeudo = true;
+  pseudo = "";
 
-  get userWantToPlayTetris() {
-    return this.playerChosePSeudo && (this.userWantJoinLobby || this.userWantDebug)
+  userWantJoinLobby = false;
+  userWantDebug = false;
+
+  showDialogAskPseudo = true;
+
+  get showOptionsToPlayer() {
+    return !(this.userWantJoinLobby || this.userWantDebug);
   }
 
-  debugLaunchParty() {
-    userWantDebug = true
+  get showLobbyToPlayer() {
+    return this.userWantJoinLobby;
+  }
+
+  mounted() {
+    this.pseudo = this.getPseudo();
+    if (this.pseudo !== "") {
+      this.showDialogAskPseudo = false;
+    }
   }
 
   joinLobbyArea() {
-    this.showDialogAskPseudo = true;
-    this.userWantPlayTetris = true;
+    this.userWantJoinLobby = true;
+  }
+  
+  debugLaunchParty() {
+    userWantDebug = true;
   }
 
-  closeDialog(){
+  closeDialog() {
     this.pseudo = "";
     this.showDialogAskPseudo = false;
   }
 
   enterDialog() {
+    this.$store.commit("setOwnPseudo", this.pseudo);
     this.showDialogAskPseudo = false;
-    // TODO: Verif pseudo is filled by player
-    this.playerChosePSeudo = true;
   }
 
+  getPseudo() {
+    return this.$store.getters.getOwnPseudo;
+  }
 }
 </script>
