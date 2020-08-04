0e6303f8d39be967e4e602283cf4e57ac76662cd
Furvent <furventt@gmail.com>
2020-08-04 10:15:22 +0200

    Server: in private logger check if env is test

--- server/src/private-module/PrivateLogger.ts
+++ server/src/private-module/PrivateLogger.ts
@@ -2,22 +2,22 @@ import colors from "colors";
 
 export default {
   error: (message: string) => {
-    if (global.globalConfig.privateLogger.error) {
+    if (process.env.NODE_ENV === "test" || global.globalConfig.privateLogger.error) {
       console.error(colors.red.bgWhite(message));
     }
   },
   info: (message: string) => {
-    if (global.globalConfig.privateLogger.info) {
+    if (process.env.NODE_ENV === "test" || global.globalConfig.privateLogger.info) {
       console.log(colors.blue(message));
     }
   },
   highlight: (message: string) => {
-    if (global.globalConfig.privateLogger.highlight) {
+    if (process.env.NODE_ENV === "test" || global.globalConfig.privateLogger.highlight) {
       console.log(colors.green(message));
     }
   },
   debug: (message: string) => {
-    if (global.globalConfig.privateLogger.debug) {
+    if (process.env.NODE_ENV === "test" || global.globalConfig.privateLogger.debug) {
       console.log(colors.magenta(message));
     }
   },
