443b6dffc8da77a010836895ca4413843c0d0bf6
Furvent <furventt@gmail.com>
2020-09-13 17:59:28 +0200

    Add function in TetrisPartyPositionsUtils

--- server/src/tetris/TetrisPartyPositionsUtils.ts
+++ server/src/tetris/TetrisPartyPositionsUtils.ts
@@ -12,10 +12,15 @@ export function placeNewTetromino(
   // Determinate the top left corner of tetromino area (see TetrominoBlueprint to get explanation)
   // It depends to the board dimension
   const tetrominoAreaTopLeftPositionOnBoard = {
-    x: Math.round(boardDimension.width / 2 - newTetromino.blueprint.side / 2) + 1, // We add '1' because it's 'x' cells of space and we want the next one
+    x:
+      Math.round(boardDimension.width / 2 - newTetromino.blueprint.side / 2) +
+      1, // We add '1' because it's 'x' cells of space and we want the next one
     y: 1, // We want new tetromino to be at the top of the board
   };
-  determinateTetrominoPositionOnBoard(newTetromino, tetrominoAreaTopLeftPositionOnBoard);
+  determinateTetrominoPositionOnBoard(
+    newTetromino,
+    tetrominoAreaTopLeftPositionOnBoard
+  );
 }
 
 /**
@@ -27,7 +32,7 @@ export function placeNewTetromino(
 export function determinateTetrominoPositionOnBoard(
   tetromino: Tetromino,
   tetrominoAreaOriginPosition: GamePosition
-) {
+): void {
   // We find in blueprint shapes the good one, determinate by direction
   const shape = tetromino.getCurrentShape();
   tetromino.currentPosition = shape.map((shapePos) => {
@@ -38,7 +43,27 @@ export function determinateTetrominoPositionOnBoard(
   });
 }
 
+export function moveTetrominoWithVector(
+  tetromino: Tetromino,
+  vector: Vector
+): void {
+  if (tetromino.currentPosition.length <= 0) {
+    throw 'In moveTetrominoWithVector(), cannot move a tetromino if no current position'
+  }
+  tetromino.currentPosition = tetromino.currentPosition.map(pos => {
+    return {
+      x: pos.x + vector.x,
+      y: pos.y + vector.y
+    }
+  })
+}
+
 export type BoardDimension = {
   width: number;
   height: number;
+};
+
+export type Vector = {
+  x: number;
+  y: number;
 };
